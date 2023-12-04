e1d41d2e7a15a0c300cdad624b06f23f2b0a0bb9
rdinit <28570294+rdinit@users.noreply.github.com>
2023-12-04 22:57:36 +0300

    change naming

--- generate_cmakelists.py
+++ generate_cmakelists.py
@@ -3,10 +3,11 @@ import os
 def run_in_directory(start_dir):
     executables = []
     links = []
+    dirtype = start_dir.split("_")[0]
     for file in os.listdir(start_dir):
         if file.endswith('cpp'):
-            executables.append(f'add_executable({file[:-4]} {file})')
-            links.append(f'target_link_libraries({file[:-4]} {start_dir.split("_")[0]})')
+            executables.append(f'add_executable({dirtype}_{file[:-4]} {file})')
+            links.append(f'target_link_libraries({dirtype}_{file[:-4]} {dirtype})')
     with open(start_dir + '/CMakeLists.txt', 'w') as f:
         f.write('\n'.join(executables + links))
 
--- run_tests.py
+++ run_tests.py
@@ -5,7 +5,7 @@ def run_in_directory(start_dir):
     start_dir += '/bin'
     texts = []
     for file in os.listdir(start_dir):
-        texts.append('tests ' + start_dir + '/' + file)
+        texts.append('\TESTS ' + start_dir + '/' + file)
         texts.append(subprocess.Popen([start_dir + '/' + file], stdout=subprocess.PIPE).communicate()[0])
     return texts.copy()
 
