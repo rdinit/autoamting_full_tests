a50c78493d60939d81c8b05838f4177d24a8ed4d
rdinit <28570294+rdinit@users.noreply.github.com>
2023-12-23 15:09:05 +0300

    new io tests

--- rational_tests/rdinit.cpp
+++ rational_tests/rdinit.cpp
@@ -1,9 +1,9 @@
 #include <rational/rational.hpp>
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest.h"
 #include <iostream>
 #include <sstream>
 
+#include "doctest.h"
 
 void testing() {
   Rational r(int64_t(3));
@@ -46,6 +46,15 @@ TEST_CASE("rational gcd") {
   Rational r_minus(-15, 6);
   CHECK(-5 == r_minus.num());
   CHECK(2 == r_minus.den());
+
+  Rational r_minus2(15, 6);
+  r_minus2 /= -1;
+  CHECK(-5 == r_minus2.num());
+  CHECK(2 == r_minus2.den());
+
+  r_minus2 *= -1;
+  CHECK(5 == r_minus2.num());
+  CHECK(2 == r_minus2.den());
 }
 
 TEST_CASE("arithmetics") {
@@ -61,17 +70,42 @@ TEST_CASE("arithmetics") {
 };
 
 TEST_CASE("IO") {
-  std::istringstream istream("2 / 3 "); //?
+  std::istringstream istream("2/3");
   Rational reading;
   Rational r23(2, 3);
 
   istream >> reading;
-  CHECK(reading == r23);
-  istream.str("-3|3");
-  istream >> reading;
-  CHECK(istream.failbit == std::ios_base::failbit);
+  CHECK_FALSE(istream.fail());
+  CHECK(reading.num() == 2);
+  CHECK(reading.den() == 3);
 
+  std::istringstream istream2("4/5");
+  istream2 >> reading;
+  CHECK(reading.num() == 4);
+  CHECK(reading.den() == 5);
+
+  std::istringstream istream3("7/8");
+  istream3 >> reading;
+  CHECK(reading.num() == 7);
+  CHECK(reading.den() == 8);
+
+  std::istringstream istream4("-712/38");
+  istream4 >> reading;
+  CHECK(reading.num() == -712);
+  CHECK(reading.den() == 38);
+
+  std::istringstream istream5("-123 / 48");
+  istream5 >> reading;
+  CHECK(reading.num() == -123);
+  CHECK(reading.den() == 48);
+
+  std::istringstream istream6("7/ -8");
+  CHECK_THROWS(istream6 >> reading);
+
+  std::istringstream istream7("4|54");
+  istream7 >> reading;
   std::ostringstream ostream;
-  ostream << Rational(2,3);
+  CHECK(istream7.fail());
+  ostream << Rational(2, 3);
   CHECK(ostream.str() == "2/3");
 };
